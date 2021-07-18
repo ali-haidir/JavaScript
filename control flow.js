dc454d68bd78991c1cf9fa65620ec57ab2717187
Ali Haider <khalihaider8@gmail.com>
2021-07-19 03:55:58 +0500

    Add files via upload

---
+++ control flow.js
@@ -0,0 +1,69 @@
+// for loop 
+
+/*
+const names = ["ali ", "aleena", "lalalla", "frog"];
+
+for( let i=0 ; i<names.length; i++){
+    console.log(names[i]);
+}
+
+//while loop 
+let i=0 
+while(i<5){
+    console.log(i);
+    i++;
+}
+
+// do while loop 
+
+let i= 4;
+do{
+    console.log("val of loop is ", i);
+    i++;
+}while(i<5)
+
+// if statement 
+
+let age = 19;
+if(age < 20 ){
+    console.log("you are below 20 years ");
+}
+
+const pass = "pass";
+if (pass.length >= 8){
+    console.log("this is a good password ");
+} 
+else{
+    console.log("its not long enogh ");
+}
+
+// logical operators || and && and !
+
+const pass = "passueh@dwieuhfiw";
+if (pass.length >= 8 && pass.includes("@")){
+    console.log("this is a good password ");
+} 
+else{
+    console.log("its not long enogh ");
+}
+*/
+
+// break and continue 
+
+// switch statement 
+
+const grade = "D";
+
+switch(grade){
+    case "A":
+        console.log("exeilent ");
+
+    case "b":
+        console.log("not exeilent ");
+    case "D":
+        console.log("you passed ");
+        break;
+    default:
+        console.log("you failed ");
+    
+}
---
+++ sandbox.js
@@ -0,0 +1,50 @@
+alert('hello world');
+ let age =25;
+ let name = "jaspn ";
+ console.log(age, name);
+ // let allows us to create a var whose value can later change 
+
+ const points = 100 ;
+
+ //points = 120 ;
+ console.log(points);
+
+ // const allows us to create variable whose value will not change 
+
+ let string = " lalalallalla";
+ console.log(string[4])
+  // we can use squar brakets after a string to get specific character
+
+
+ // NaN error thats means not a number eg 5 / likes 
+
+ //cancenating numbers
+ const title = "best reads ";
+ const likes = 49;
+ 
+ let result = `${title} has scoured ${likes} likes this month`;
+ console.log(result);
+ //this is the templet way of cancatination 
+
+
+ let age; 
+ console.log(age , age +3)
+
+ // undifined value
+
+ let age = null;
+
+ //null value  
+
+
+ // strick comparision is === or !==
+
+
+ // type conversion 
+ let score = 100;
+
+ score = Number(score);
+
+ // positive numbers and negitive numbers = positive values 
+ //0 is negitive number
+ 
